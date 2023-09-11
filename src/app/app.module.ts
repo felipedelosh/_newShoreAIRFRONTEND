f2e44dcac22ed5e73e5ae9cedaf4d3c30770c595
felipedelosh <doctorfhernandez@hotmail.com>
2023-09-11 18:11:03 -0500

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContentPageComponent } from './components/content-page/content-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentPageComponent
   ],
   imports: [
     BrowserModule
